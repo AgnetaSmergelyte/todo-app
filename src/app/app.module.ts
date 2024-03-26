73ddffd04a7158b9b94519d9c43f66f52a4c6422
spegisima <agneta.smergelyte@gmail.com>
2024-03-26 17:53:00 +0200

    router deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodosComponent } from './components/todos/todos.component';
-import { FormsModule } from "@angular/forms";
 
+import { FormsModule } from "@angular/forms";
 import { NgIconsModule } from '@ng-icons/core';
 import { heroTrash, heroPencilSquare, heroPencil, heroClipboardDocumentList, heroClipboardDocumentCheck, heroCheck, heroCheckCircle } from '@ng-icons/heroicons/outline';
 
@@ -16,7 +15,6 @@ import { heroTrash, heroPencilSquare, heroPencil, heroClipboardDocumentList, her
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     FormsModule,
     NgIconsModule.withIcons({ heroTrash, heroPencilSquare, heroPencil, heroClipboardDocumentList, heroClipboardDocumentCheck, heroCheck, heroCheckCircle }),
   ],
