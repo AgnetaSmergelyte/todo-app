324434c62191fc83772420e3c335ff91513f18e1
AgnetaSmergelyte <agneta.smergelyte@gmail.com>
2025-03-14 13:07:12 +0200

    pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -7,6 +7,7 @@ import { TodosComponent } from './components/todos/todos.component';
 import { FormsModule } from "@angular/forms";
 import { NgIconsModule } from '@ng-icons/core';
 import { heroTrash, heroPencilSquare, heroPencil, heroClipboardDocumentList, heroClipboardDocumentCheck, heroCheck, heroCheckCircle } from '@ng-icons/heroicons/outline';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -17,6 +18,12 @@ import { heroTrash, heroPencilSquare, heroPencil, heroClipboardDocumentList, her
     BrowserModule,
     FormsModule,
     NgIconsModule.withIcons({ heroTrash, heroPencilSquare, heroPencil, heroClipboardDocumentList, heroClipboardDocumentCheck, heroCheck, heroCheckCircle }),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
